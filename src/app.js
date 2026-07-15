1a0e7594fb2bce3d7a8058182c5e8a2b32ef8685
Roushan225 <roushan2029@gmail.com>
2026-07-15 20:10:02 +0530

    testing backend of mushroom app

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes will be mounted here
+// Routes will be mounted 
 app.use('/api/v1/auth', require('./routes/authRoutes'));
 app.use('/api/v1/products', require('./routes/productRoutes'));
 app.use('/api/v1/orders', require('./routes/orderRoutes'));
